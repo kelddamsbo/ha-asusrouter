a0075758b0166410507a0470c19d97133f569622
kelddamsbo <121604835+kelddamsbo@users.noreply.github.com>
2026-09-07 18:08:29 +0200

    Refactor via_device to use device registry
    Update device info to use device registry for via_device_id.

--- custom_components/asusrouter/binary_sensor.py
+++ custom_components/asusrouter/binary_sensor.py
@@ -14,6 +14,7 @@ from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers import device_registry as dr
 
 from .const import (
     AIMESH,
@@ -164,7 +165,13 @@ class AMBinarySensor(BinarySensorEntity):
                 model=self._node.native.model,
                 manufacturer=MANUFACTURER,
                 sw_version=self._node.native.fw,
-                via_device=(DOMAIN, self._router.mac),
+                via_device_id=(
+                    dr.async_get(self.hass)
+                    .async_get_device(
+                        identifiers={(DOMAIN, self._router.mac)}
+                    )
+                    .id
+                ),
             )
 
         return device_info
