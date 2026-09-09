2585dd4bfb7d3eaec004b81ce3b366eaba0caeea
kelddamsbo <121604835+kelddamsbo@users.noreply.github.com>
2026-09-09 18:24:11 +0200

    Fix device_id retrieval in binary_sensor.py

--- custom_components/asusrouter/binary_sensor.py
+++ custom_components/asusrouter/binary_sensor.py
@@ -163,7 +163,7 @@ class AMBinarySensor(BinarySensorEntity):
                     for identifier in self._router.bridge.identifiers
                     if (
                         device_id := dr.async_get_device_id_by_identifier(
-                            self.hass,
+                            self._router.hass,
                             identifier,
                             config_entry_id=self._router._config_entry.entry_id,
                         )
