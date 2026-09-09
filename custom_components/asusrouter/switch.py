7202794afcaabad7a588d32fe20a0c3406683864
kelddamsbo <121604835+kelddamsbo@users.noreply.github.com>
2026-09-09 17:35:21 +0200

    Enhance device info compilation with router ID
    Refactor _compile_device_info to include router_device_id.

--- custom_components/asusrouter/switch.py
+++ custom_components/asusrouter/switch.py
@@ -169,11 +169,31 @@ class ClientInternetSwitch(SwitchEntity):
     def _compile_device_info(self) -> DeviceInfo:
         """Compile device info."""
   
-        return DeviceInfo(
+        device_info: DeviceInfo = DeviceInfo(
             connections={(dr.CONNECTION_NETWORK_MAC, self._mac)},
             name=self._rule.name,
-            via_device=(DOMAIN, self._router.mac),
         )
+
+        router_device_id = next(
+            (
+                device_id
+                for identifier in self._router.bridge.identifiers
+                if (
+                    device_id := dr.async_get_device_id_by_identifier(
+                        self.hass,
+                        identifier,
+                        config_entry_id=self._router._config_entry.entry_id,
+                    )
+                )
+                is not None
+            ),
+            None,
+        )
+
+        if router_device_id is not None:
+            device_info["via_device_id"] = router_device_id
+
+        return device_info
 
     @property
     def is_on(self) -> bool | None:
